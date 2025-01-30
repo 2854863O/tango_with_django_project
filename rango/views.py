f32d23a2ea5d9930b397fe2e4342658cd179bfee
“David <2854863O@student.gla.ac.uk>
2025-01-30 16:08:53 +0000

    3rd commit, first hyperlink between about and index

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index (request):
-    return HttpResponse("Rango says hey there partner")
+    about_link = "<a href='/rango/about'>About</a>"
+    return HttpResponse("Rango says hey there partner! Here is a link to the about page - " +about_link)
+
+def about (request):
+    rango_link = "<a href='/rango/'>Rango</a>"
+    return HttpResponse('Rango says here is the about page.'+ rango_link)
