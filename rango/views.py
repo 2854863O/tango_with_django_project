be04e1ab38ca9813bb0b651b321e08182c08d593
“David <2854863O@student.gla.ac.uk>
2025-02-08 13:07:43 +0000

    chapter 8 done before tests and exercises

--- rango/views.py
+++ rango/views.py
@@ -42,7 +42,7 @@ def add_category(request):
             return redirect("/rango/")
         else:
             print(form.errors)
-            return render(request, "rango/add_category.html", {"form": form, "errors": form.errors})  # Show errors in template
+            return render(request, "rango/add_category.html", {"form": form, "errors": form.errors})  
     return render(request, 'rango/add_category.html',{'form':form}) 
 
 def add_page(request, category_name_slug):
